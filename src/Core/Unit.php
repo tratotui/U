27b2a01aa23c1132756fc6c4739338b6caa37b25
Oleg Volodin <adm@ausite.ru>
2016-07-19 14:35:48 +0300

    Update framework

--- src/Core/Unit.php
+++ src/Core/Unit.php
@@ -4,8 +4,55 @@ namespace U\Core;
 
 class Unit
 {
+
+	const GETTER = 'getter';
+
+	/**
+	 * Array with properties of unit
+	 */
+	protected $properties = [];
+
 	public function __construct()
 	{
-		echo 'Hello world';
+
 	}
+
+	public function __get($prop)
+	{
+		$_method = null;
+		if(
+			!array_key_exists($prop, $this->properties) && 
+			!method_exists($this, $_method = self::GETTER . ucfirst($prop))
+		)
+		{
+			return null;
+		}
+
+		return !is_null($_method) ? call_user_func([$this, $_method]) : $this->properties[$prop];
+	}
+
+	public function __set($prop, $val)
+	{
+		$this->properties[$prop] = $val;
+	}
+
+	public function getterFull_name()
+	{
+		return 'Hello';
+	}
+
+
+
+
 }
+/*
+
+$u = new Unit;
+
+$u->full_name = 2131
+
+$u->full_name = 3213213;
+
+
+
+*/
